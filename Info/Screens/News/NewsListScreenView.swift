8a015ea0995bf5eb796cec62e8b5db4ae618d1a6
Mikhail.Zinovev <mikhail.zinovev@russianpost.ru>
2023-04-02 17:58:25 +0300

    Add navigationView for news screen

--- Info/Screens/News/NewsListScreenView.swift
+++ Info/Screens/News/NewsListScreenView.swift
@@ -10,36 +10,31 @@ import SwiftUI
 struct NewsListScreenView: View {
     
     @StateObject var viewModel: NewsListViewModel
+    @State var isPresented: Bool = false
     
     init(viewModel: NewsListViewModel) {
         _viewModel = StateObject(wrappedValue: viewModel)
     }
     
     var body: some View {
-        VStack {
-            TextField("Ваш запрос", text: $viewModel.searchText)
-                .textFieldStyle(.roundedBorder)
-                .padding(16)
-                .onSubmit {
-                    viewModel.performSearch()
-                }
-            LoadingView(isShowing: $viewModel.isSearchEnabled) {
-                List {
-                    Section {
+        NavigationView {
+            VStack {
+                TextField("Ваш запрос", text: $viewModel.searchText)
+                    .textFieldStyle(.roundedBorder)
+                    .padding(16)
+                    .onSubmit {
+                        viewModel.performSearch()
+                    }
+                LoadingView(isShowing: $viewModel.isSearchEnabled) {
+                    List {
                         ForEach($viewModel.articles) { data in
                             NewsListView(data: data)
-                                .onTapGesture {
-                                    guard let string = data.article.url as? String, let url = URL(string: string ) else { return }
-                                    NavigationLink("Детали") {
-                                        WebView(url: url)
-                                    }
-                            }
                         }
-                    }
-                }.resignKeyboardOnDragGesture()
+                    }.resignKeyboardOnDragGesture()
+                }
+            }.onViewDidLoad {
+                viewModel.resetSearch()
             }
-        }.onViewDidLoad {
-            viewModel.resetSearch()
         }
     }
 }
