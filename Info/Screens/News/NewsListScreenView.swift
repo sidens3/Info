1b62d08a31f114cf2dbb7818b7c5726ff11ebcfb
Mikhail.Zinovev <mikhail.zinovev@russianpost.ru>
2023-04-02 14:34:39 +0300

    add keyboard hide view modifier

--- Info/Screens/News/NewsListScreenView.swift
+++ Info/Screens/News/NewsListScreenView.swift
@@ -29,7 +29,7 @@ struct NewsListScreenView: View {
                         NewsListView(data: article)
                     }
                 }
-            }
+            }.resignKeyboardOnDragGesture()
         }.onViewDidLoad {
             viewModel.resetSearch()
         }
