1132726e57e2f42a3330f6033ae541f16c77ee71
Mikhail.Zinovev <mikhail.zinovev@russianpost.ru>
2023-04-02 13:55:32 +0300

    fix ui for News List view

--- Info/View/News/NewsListView.swift
+++ Info/View/News/NewsListView.swift
@@ -15,25 +15,21 @@ struct NewsListView: View {
         HStack {
             KFImage(URL(string: data.article.urlToImage ?? ""))
                 .resizable()
-                .scaledToFit()
                 .frame(width: 60, height: 60, alignment: .center)
-                .clipShape(RoundedRectangle(cornerRadius: 16))
+                .cornerRadius(16)
                 .padding(.trailing, 16)
             Text(data.article.title)
                 .foregroundColor(.primary)
                 .fontWeight(.regular)
                 .multilineTextAlignment(.leading)
                 .textSelection(.disabled)
-        }.padding(.init(top: 8,
-                        leading: 16,
-                        bottom: 8,
-                        trailing: 16))
+        }
     }
 }
 
-//struct NewsListView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        @State var article = Article.getDefault()
-//        NewsListView(article: $article )
-//    }
-//}
+struct NewsListView_Previews: PreviewProvider {
+
+    static var previews: some View {
+        StatefulPreviewWrapper(ArticleListObject(article: Article.getDefault())) { NewsListView(data: $0 ) }
+    }
+}
